d22db730f8f1abc05ac0a031828852a615ce3321
Davelpz-tech <davelpz15@gmail.com>
2021-05-21 19:22:25 -0700

    adding button function to present password options

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,19 @@
 // Assignment code here
+var passOptions = [
+  {q: 'Does your password include Uppercase?', a: 't'},
+  {q: 'Does your password include Lowercase?', a: 't'},
+  {q: 'Does your password include Numbers?', a: 't'},
+  {q: 'Does your password include Symbols?', a: 't'},
+]
+function options(){
+  for (var i = 0; i < passOptions.length; i++) {
+    var userInput = confirm(passOptions[i].q);
+    if (
+      (userInput === true && passOptions[i].a === 't') || (userInput === false && passOptions[i].a === 'f')) {
 
+      }
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +29,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+generateBtn.addEventListener("click", options);
+
