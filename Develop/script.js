91e5ee9c8ae4e54eb53ca227a190031ef2727156
Davelpz-tech <davelpz15@gmail.com>
2021-05-23 02:33:55 -0700

    final code

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@ var addNumber = document.querySelector("#addNumber");
 var valOptions = document.getElementById('options');
 var validate = 0;
 
-
+// Make sure at least one of the check boxes is selected and making sure range of characters is set
 valOptions.onsubmit = function(e){
   e.preventDefault();
   if(addNumber.checked === true){
@@ -17,39 +17,54 @@ valOptions.onsubmit = function(e){
   } if(rangeChar.value.length == 0){
     validate = 0;
   } if(validate == 1) {
-    writePassword(rangeChar.value);
+    writePassword(rangeChar.value, addUpper.checked, addLower.checked, addSymbols.checked, addNumber.checked);
   } else {
     alert('Either one of your check boxes are not selected or your number range is blank')
   }
 }
 
 
-//(example) var myString = "Hello "; myString += String.fromCharCode(65);
 
+//  creates password
+function generatePassword(numChar, containsUpper, containsLower, containsSymbols, containsNumbers) {
 
-function generatePassword() {
-  
+  // array that concatenates which character options you would like to include in your password
+  var charCodes = [];
+  if(containsLower) charCodes = charCodes.concat(lowercase_codes);
+  if(containsUpper) charCodes = charCodes.concat(uppercase_codes);
+  if(containsNumbers) charCodes = charCodes.concat(number_codes);
+  if(containsSymbols) charCodes = charCodes.concat(symbols_codes);
+
+  // apppends characters until 
+  var passChar = []
+  for (var i = 0; i < numChar; i++) {
+    var characterCode = charCodes[Math.floor(Math.random() * (charCodes.length -1))]
+    passChar.push(String.fromCharCode(characterCode))
+  }
+  return passChar.join('')
 }
 
+// array for ascii codes according to parameter range
+var uppercase_codes = lowToHigh(97, 122);
+var lowercase_codes = lowToHigh(65, 90);
+var number_codes = lowToHigh(48, 57);
+var symbols_codes = lowToHigh(33, 47);
 
-
-
-
-
-
-
-
-
-
-
-
+// for loop that creates array incrementing according to the parameter range
+function lowToHigh(low, high) {
+  var array = []
+  for (var i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword(numChar) {
-  var password = generatePassword();
+function writePassword(numChar, containsUpper, containsLower, containsSymbols, containsNumbers) {
+  var password = generatePassword(numChar, containsUpper, containsLower, containsSymbols, containsNumbers);
   var passwordText = document.querySelector("#password");
   console.log(numChar)
   passwordText.value = password;
